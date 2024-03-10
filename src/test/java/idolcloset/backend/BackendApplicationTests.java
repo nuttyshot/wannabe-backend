410a9fa29c88aa012cad5b0c4574bbf699dcf186
pu1et <pu1etproof@gmail.com>
2024-03-10 22:22:13 +0900

    chore: initialize spring boot based project

---
+++ src/test/java/idolcloset/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package idolcloset.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
