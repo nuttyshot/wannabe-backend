729782a731d8a7bc6ad686c35f5d031a82c70ce1
pu1et <pu1etproof@gmail.com>
2024-04-01 20:50:11 +0900

    refactor: 프로젝트 네이밍 변경

--- src/main/java/wannabe/backend/BackendApplication.java
+++ src/main/java/wannabe/backend/BackendApplication.java
@@ -1,4 +1,4 @@
-package idolcloset.backend;
+package wannabe.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/wannabe/backend/BackendApplicationTests.java
+++ src/test/java/wannabe/backend/BackendApplicationTests.java
@@ -1,4 +1,4 @@
-package idolcloset.backend;
+package wannabe.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
