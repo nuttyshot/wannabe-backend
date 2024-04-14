7c0f1035132ebab54a22516f69464965dbf2467c
pu1et <pu1etproof@gmail.com>
2024-04-14 20:18:39 +0900

    feat: 카카오 인가 코드 받는 controller 추가

---
+++ src/main/java/wannabe/backend/web/oauth2/OAuth2Controller.java
@@ -0,0 +1,40 @@
+package wannabe.backend.web.oauth2;
+
+import java.util.UUID;
+import javax.naming.OperationNotSupportedException;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.val;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
+import wannabe.backend.core.oauth2.OAuth2ArgumentChooserService;
+
+@RestController
+@RequestMapping("/oauth2/authorization")
+@RequiredArgsConstructor
+public class OAuth2Controller {
+
+  private final OAuth2ArgumentChooserService chooser;
+
+  @GetMapping("/kakao")
+  public RedirectView kakao() {
+    return new RedirectView(redirectUri("kakao"));
+  }
+
+  private String redirectUri(@NonNull String registrationId) {
+    try {
+      val argumentPort = chooser.choose(registrationId);
+      return String.format("%s?response_type=code&client_id=%s&state=%s&redirect_uri=%s&scope=%s",
+          argumentPort.client().provider().authorizationUri(),
+          argumentPort.client().registration().clientId(),
+          UUID.randomUUID(),
+          argumentPort.client().registration().redirectUri(),
+          argumentPort.client().registration().scope().stream().reduce((a, b) -> a + "," + b).get()
+      );
+    } catch (OperationNotSupportedException e) {
+      throw new IllegalStateException(e);
+    }
+  }
+}
