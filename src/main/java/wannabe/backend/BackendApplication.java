89169d09bce3a5b402b8fca7e88b93c5a5d03176
pu1et <pu1etproof@gmail.com>
2024-04-16 11:16:38 +0900

    feat: 타임존 Asia/Seoul 설정

--- src/main/java/wannabe/backend/BackendApplication.java
+++ src/main/java/wannabe/backend/BackendApplication.java
@@ -1,5 +1,7 @@
 package wannabe.backend;
 
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,4 +12,8 @@ public class BackendApplication {
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 }
